998f934ce072c91cab1647eadebb0681620a865e
DDstar1 <abhuluimendesiny@gmail.com>
2025-06-25 16:12:18 +0100

    ve

--- config.py
+++ config.py
@@ -2,5 +2,3 @@
 BASE_URL = "https://africa.espn.com"
 SCRAPE_END_YR = 2023
 WHICH_DB = 'db.db_supab_utils'
-SUPABASE_URL = "https://vxsflheqcgeafumytqgm.supabase.co" 
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4c2ZsaGVxY2dlYWZ1bXl0cWdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzOTA4NjYsImV4cCI6MjA2MTk2Njg2Nn0.AuIqGGY2DzrJVqcj9yX5wGnp4kggXHuAUJBHtHCSmws"  # Replace with your Supabase service role key
